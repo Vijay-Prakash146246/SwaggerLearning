3a955e0ba4ec24f88699a2e1bbf44958015094b2
Vijay-Prakash146246 <vp146246@gmail.com>
2023-08-09 14:27:35 +0530

    Swagger Configuration get added along with different kind of Annotation

---
+++ LoadBalancer/src/main/java/com/LoadBalancer/LoadBalancerApplication.java
@@ -0,0 +1,23 @@
+package com.LoadBalancer;
+
+import com.LoadBalancer.repository.UserRepository;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API Documentation",
+		description = "All User API for CRUD Operation"))
+public class LoadBalancerApplication
+{
+	@Autowired
+	private  static UserRepository userRepository;
+	public static void main(String[] args)
+	{
+		SpringApplication.run(LoadBalancerApplication.class, args);
+
+	}
+
+
+}
